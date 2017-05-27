9d3ce02ae430016bdceb652c039f987130373694
Jonathan <jonathankchin@gmail.com>
2017-05-27 12:14:10 -0400

    prep for heroku w/ express

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.set('port', (process.env.PORT || 3001));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
