af85f8059eef9c23275cf686494850e57be92dbb
Jonathan <jonathankchin@gmail.com>
2017-05-29 11:39:58 -0400

    creating routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,32 @@
 const routes = require('express').Router();
+const _ = require('lodash');
 
 const {ObjectID} = require('mongodb');
 const {Article} = require('./../models/article');
 
-routes.get('/api/articles', (req, res) => {
+routes.get('/api/test', (req, res) => {
     console.log("you've hit the route")
     res.send("This works")
+});
+
+routes.get("/api/articles", (req, res) => {
+    Article.find({}, (err, articles) => {
+        err ? res.status(400).send() : res.json(articles)
+    })
+})
+
+routes.post("/api/articles", (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        url: req.body.url,
+        description: req.body.description
+    });
+
+    article.save().then((item) => {
+        res.send(item);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 })
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
 
 const routes = require('./routes/routes');
 
-
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.set('port', (process.env.PORT || 3001));
-
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+mongoose.Promise = Promise;
+
+const dbUri = process.env.MONGODB_URI || "mongodb://localhost/nytimes-react";
+
+mongoose.connect(dbUri).then(() => console.log('connected to DB!')).catch((err) => console.log(err));
 
 app.use('/', routes)
 
